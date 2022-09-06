e64a19f4594325ca0a26c772a11873b70b27fd8f
sarang2551 <74551805+sarang2551@users.noreply.github.com>
2022-09-06 11:04:35 +0800

    Modified the search engine to have weighted search vectors and relevancy ranking

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -8,7 +8,7 @@ from .forms import EmailPostForm, CommentForm, SearchForm
 from django.core.mail import send_mail
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 # django workflow
 # python component --> view --> registering url --> template
@@ -102,5 +102,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search = SearchVector('title','body')).filter(search=query) # List of posts that satisfy the search field
+            search_vector = SearchVector('title',weight='A') + SearchVector('body',weight='B') # Body relevnancy has a higher weight than body relevancy
+            search_query = SearchQuery(query)
+            # Search Rank to list the posts based on relevancy
+            results = Post.published.annotate(search = search_vector,rank = SearchRank(search_vector,search_query)).filter(search=search_query).order_by('-rank') # List of posts that satisfy the search field
     return render(request,'blog/post/search.html',{'form':form, 'query':query, 'results':results})
